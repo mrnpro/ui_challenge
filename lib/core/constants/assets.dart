5df7a002dbc27b05d913ccb9feaf6025c3ffdbbb
Abraham Ayana <yegetanaty03@gmail.com>
2023-10-13 18:06:49 +0300

    push assets

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,35 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsPicturesPlant1
+  /// assets/pictures/plant1.png
+  static const String assetsPicturesPlant1 = "assets/pictures/plant1.png";
+
+  /// Assets for assetsPicturesPlant2
+  /// assets/pictures/plant2.png
+  static const String assetsPicturesPlant2 = "assets/pictures/plant2.png";
+
+  /// Assets for assetsPicturesPlant3
+  /// assets/pictures/plant3.png
+  static const String assetsPicturesPlant3 = "assets/pictures/plant3.png";
+
+  /// Assets for assetsSvgBagFillIcon
+  /// assets/svg/bag_fill_icon.svg
+  static const String assetsSvgBagFillIcon = "assets/svg/bag_fill_icon.svg";
+
+  /// Assets for assetsSvgFilterIcon
+  /// assets/svg/filter_icon.svg
+  static const String assetsSvgFilterIcon = "assets/svg/filter_icon.svg";
+
+  /// Assets for assetsSvgPotIcon
+  /// assets/svg/pot_icon.svg
+  static const String assetsSvgPotIcon = "assets/svg/pot_icon.svg";
+
+  /// Assets for assetsSvgSearchLightIcon
+  /// assets/svg/search_light_icon.svg
+  static const String assetsSvgSearchLightIcon = "assets/svg/search_light_icon.svg";
+
+  /// Assets for assetsSvgTemperatureIcon
+  /// assets/svg/temperature_icon.svg
+  static const String assetsSvgTemperatureIcon = "assets/svg/temperature_icon.svg";
+}
