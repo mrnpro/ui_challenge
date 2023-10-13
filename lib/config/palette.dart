9dd6377a8d965aa05cb2a46d91c0a621cef30720
Abraham Ayana <yegetanaty03@gmail.com>
2023-10-13 18:07:15 +0300

    Create palette.dart

---
+++ lib/config/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import '../core/utils/color_util.dart';
+
+final class MyColorPalette {
+  static final kPrimaryColor = HexColor('F6F6F6');
+  static final kSecondaryColor = HexColor('76984C');
+  static final kThirdColor = HexColor('67864D');
+}
