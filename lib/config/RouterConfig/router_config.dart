5dbf40b634755f9b9f8333b93e21627216fd43d3
Abraham Ayana <yegetanaty03@gmail.com>
2023-10-14 19:47:57 +0300

    add route screen

--- lib/config/RouterConfig/router_config.dart
+++ lib/config/RouterConfig/router_config.dart
@@ -1,5 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:plant_app/config/RouterConfig/route_names.dart';
+import 'package:plant_app/plant_data.dart';
+import 'package:plant_app/presentation/screens/DetailScreen/detail_screen.dart';
 import '../../presentation/screens/OnboardingScreen/onboarding_screen.dart';
 import '../../presentation/screens/SearchScreen/search_screen.dart';
 import 'route_transition.dart';
@@ -19,6 +21,14 @@ class RouteConfig {
             key: state.pageKey,
             child: const SearchScreen(),
           ),
+        ),
+        GoRoute(
+          path: RouteNames.detailScreen,
+          pageBuilder: (context, state) => FadeTransitionPage(
+              key: state.pageKey,
+              child: DetailScreen(
+                plant: state.extra as PlantModel,
+              )),
         )
       ]);
 }
