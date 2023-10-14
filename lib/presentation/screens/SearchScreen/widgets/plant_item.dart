23a76411c084021eb16d6889d01c2f67a7fe9219
Abraham Ayana <yegetanaty03@gmail.com>
2023-10-14 19:48:41 +0300

    Update plant_item.dart

--- lib/presentation/screens/SearchScreen/widgets/plant_item.dart
+++ lib/presentation/screens/SearchScreen/widgets/plant_item.dart
@@ -8,70 +8,78 @@ class PlantItem extends StatelessWidget {
   const PlantItem({
     super.key,
     required this.plant,
+    required this.onPressed,
   });
   final PlantModel plant;
+  final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        height: 300,
-        decoration: BoxDecoration(boxShadow: const [
-          BoxShadow(
-            color: Color.fromARGB(255, 233, 233, 233), // Shadow color
-            spreadRadius: 1, // Spread radius
-            blurRadius: 2, // Blur radius
-            offset: Offset(0, 4), // Offset in the x and y directions
-          ),
-        ], borderRadius: BorderRadius.circular(20), color: Colors.white),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Image.asset(
-              plant.image,
-              width: 140.w,
-              height: 170.h,
-              fit: BoxFit.contain,
+      child: InkWell(
+        onTap: onPressed,
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          height: 300,
+          decoration: BoxDecoration(boxShadow: const [
+            BoxShadow(
+              color: Color.fromARGB(255, 233, 233, 233), // Shadow color
+              spreadRadius: 1, // Spread radius
+              blurRadius: 2, // Blur radius
+              offset: Offset(0, 4), // Offset in the x and y directions
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(plant.name,
-                        style: TextStyle(
-                          fontSize: 14.sp,
-                          fontWeight: FontWeight.bold,
-                        )),
-                    Text(plant.subTitle,
-                        style: const TextStyle(
-                          fontSize: 11,
-                        )),
-                  ],
+          ], borderRadius: BorderRadius.circular(20), color: Colors.white),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Hero(
+                tag: plant.id,
+                child: Image.asset(
+                  plant.image,
+                  width: 140.w,
+                  height: 170.h,
+                  fit: BoxFit.contain,
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text("\$12.99",
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold,
-                    )),
-                CircleAvatar(
-                    radius: 15,
-                    backgroundColor: Colors.black,
-                    child: Icon(
-                      Icons.favorite,
-                      color: MyColorPalette.kPrimaryColor,
-                      size: 20,
-                    ))
-              ],
-            )
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(plant.name,
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      Text(plant.subTitle,
+                          style: const TextStyle(
+                            fontSize: 11,
+                          )),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text("\$12.99",
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Colors.black,
+                      child: Icon(
+                        Icons.favorite,
+                        color: MyColorPalette.kPrimaryColor,
+                        size: 20,
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
